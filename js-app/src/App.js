87237d1a7ab766fa32d941fd347305a6b7884259
Drew Kozlov <andreigbk@gmail.com>
2022-07-08 17:03:07 +0300

    Add Clicker on new rep

---
+++ js-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Counter from "./components/clicker";
+
+function App() {
+    return (
+        <div className={"App"}>
+            <Counter>
+
+            </Counter>
+        </div>
+    );
+}
+
+export default App;
